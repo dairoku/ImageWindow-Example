bc2b53238f8c55d8d0761d9c8b9560b7a8c3dba0
Dairoku Sekiguchi <dairoku@gmail.com>
2018-07-08 14:47:36 +0900

    Added the example01 and the images directory

---
+++ example01/main.cpp
@@ -0,0 +1,54 @@
+// =============================================================================
+//	main.cpp
+//
+//	MIT License
+//
+//	Copyright (c) 2018 Dairoku Sekiguchi
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
+// =============================================================================
+/*!
+	\file		main.cpp
+	\author		Dairoku Sekiguchi
+	\version	1.0 (Release.01)
+	\date		2018/07/08
+	\brief		ImageWindow example (01)
+*/
+
+//	Includes -------------------------------------------------------------------
+#include <windows.h>
+#include <stdio.h>
+#include "ImageWindow.hpp"
+
+// -----------------------------------------------------------------------------
+//	main
+// -----------------------------------------------------------------------------
+//
+int	main(int argc, char *argv[])
+{
+	printf("ImageWindow example01\n");
+
+	ImageWindow	image("image");
+
+	image.OpenBitmapFile("lena.bmp");
+	image.ShowWindow();
+	image.WaitForWindowClose();
+
+	return 0;
+}
